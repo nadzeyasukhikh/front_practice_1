19845177311e41c519583f69d4fd4988f473b0b3
nadzeya <vitserresvit@gmail.com>
2023-08-15 16:53:38 +0300

    new

--- homework2/script.js
+++ homework2/script.js
@@ -1,19 +1,89 @@
-const section = document.querySelector('section')
+const section = document.querySelector("section");
 const cartItems = [
-    { name: 'Apple', price: 120, quantity: 3 },
-    { name: 'Banana', price: 50, quantity: 5 },
-    { name: 'Orange', price: 6, quantity: 2 },
-    { name: 'PineApple', price: 2, quantity: 2 },
-    { name: 'Coconut', price: 90, quantity: 2 },
-    { name: 'Peach', price: 45, quantity: 2 },
-]
+  { name: "Apple", price: 120, quantity: 3 },
+  { name: "Banana", price: 50, quantity: 5 },
+  { name: "Orange", price: 6, quantity: 2 },
+  { name: "PineApple", price: 2, quantity: 2 },
+  { name: "Coconut", price: 90, quantity: 2 },
+  { name: "Peach", price: 45, quantity: 2 },
+  { name: "Mango", price: 70, quantity: 4 },
+  { name: "Grapes", price: 30, quantity: 3 },
+  { name: "Watermelon", price: 120, quantity: 1 },
+  { name: "Cherry", price: 80, quantity: 2 },
+  { name: "Pear", price: 55, quantity: 3 },
+  { name: "Kiwi", price: 60, quantity: 2 },
+  { name: "Strawberry", price: 40, quantity: 5 },
+  { name: "Blueberry", price: 75, quantity: 2 },
+  { name: "Raspberry", price: 90, quantity: 3 },
+  { name: "Pomegranate", price: 100, quantity: 1 },
+  { name: "Blackberry", price: 55, quantity: 4 },
+  { name: "Apricot", price: 35, quantity: 3 },
+  { name: "Plum", price: 30, quantity: 2 },
+  { name: "Lemon", price: 10, quantity: 5 },
+  { name: "Lime", price: 15, quantity: 2 },
+  { name: "Cantaloupe", price: 80, quantity: 1 },
+  { name: "Honeydew", price: 70, quantity: 2 },
+  { name: "Fig", price: 50, quantity: 3 },
+  { name: "Guava", price: 65, quantity: 2 },
+];
 
-const cartItems1 = cartItems.map((el) => `
-<div>
-<h1>${el.name}</h1>
-<p>${el.price}</p>
-<span>${el.quantity}</span>
+function renderItems() {
+  section.innerHTML = cartItems
+    .map(
+      (el) => `
+<div class="cards">
+<h2>${el.name}</h2>
+<h4> price: ${el.price} $</h4>
+<div class="addBasc">
+<button class="btnCart" data-price="${el.price}" > </button>
 </div>
-`);
+</div>
+`
+    )
+    .join("");
+  addToBasketListeners();
+}
+renderItems();
 
-section.innerHTML = cartItems1.join('');
+const low = document.querySelector(".low");
+low.addEventListener("click", () => {
+  cartItems.sort((a, b) => a.price - b.price);
+  renderItems();
+});
+const high = document.querySelector(".high");
+high.addEventListener("click", () => {
+  cartItems.sort((a, b) => b.price - a.price);
+  renderItems();
+});
+let sum = 0;
+let total = 0;
+document.querySelector(".prdPrice").innerText = `products price: ${total} $`;
+document.querySelector(".prdQuant").innerText = `products quantity: ${sum}`;
+
+
+function addToBasketListeners() {
+  const addToBasket = document.querySelectorAll(".addBasc button");
+
+  addToBasket.forEach((button) => {
+    button.addEventListener("click", function () {
+      const price = parseFloat(this.dataset.price);
+
+      total += price;
+      sum += 1;
+      document.querySelector(
+        ".prdPrice"
+      ).innerText = `products  price: ${total} $`;
+      document.querySelector(
+        ".prdQuant"
+      ).innerText = `products quantity: ${sum}`;
+    });
+  });
+}
+
+ const cartItems2 = cartItems.map((el,ind ) => ({...el, id: ind }));
+// создать две кнопки
+// одна сортирует по увеличению
+// другая по уменьшению
+// выводить общее количество товаров
+// через функцию reduce выводить общую сумму товаров(price)
+// в карточке добаить кпопку в корзину
